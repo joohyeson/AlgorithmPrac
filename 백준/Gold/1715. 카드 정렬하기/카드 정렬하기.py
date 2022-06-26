e4a873225a781548bd7e9f292c97d2ec8662aab1
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2022-06-27 02:39:45 +0900

    Update 카드 정렬하기.py

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -9,8 +9,11 @@ for i in range(count):
     heapq.heappush(arr, now)
 sum = 0
 
-while len(arr)!= 1 :
+for i in range(count-1):
     value = heapq.heappop(arr)+heapq.heappop(arr)
     sum += value
     heapq.heappush(arr, value)
 print(sum)
+
+# 3개인 경우 : 10 20 30 (10+20)+(30+30) 2번
+# 4개인 경우 : 10 20 30 40 (10+20)+(30+30)+(30+40)
