0063620cad1ee5d969a9db007429d3bf596153d0
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2022-06-26 22:52:04 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+int data[1001];
+
+int main(void)
+{
+	int num = 0;
+
+	num = 0;
+	for (int j = 0; j < 1001; j++)
+	{
+		data[j] = 0;
+	}//init
+
+	scanf("%d", &num);
+	data[0] = 1;
+	data[1] = 1;
+	data[2] = 2;
+
+	for (int k = 3; k <= num; k++)//배열안에 카운트 값 저장
+	{
+
+		data[k] = (data[k - 1] + data[k - 2])%10007;
+
+	}
+	printf("%d\n", data[num]%10007);
+
+}
