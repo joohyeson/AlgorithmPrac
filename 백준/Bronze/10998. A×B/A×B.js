2a72d3e073ef0b4fe5327e1b8dfe00ce267d238c
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2023-05-20 13:44:55 +0900

    [Bronze V] Title: A×B, Time: 144 ms, Memory: 9460 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,28 @@
+
+function solution(list){
+    const arr=list[0].split(' ').map((num)=>parseInt(num));
+
+    console.log(arr[0]*arr[1]) ;
+}
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let list = [];
+let count=0;
+
+
+rl.on('line', function(line) {
+        list.push(line);
+        count++;
+
+        if(count===1){
+            rl.close();
+        }
+}).on("close", function() {
+    //for(let i=0; i<list.length; i++){
+        solution(list);
+    //}
+    process.exit();
+});
