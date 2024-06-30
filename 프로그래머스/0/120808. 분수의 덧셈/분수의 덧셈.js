2d4edd464b0e905a6ebdf5efa4b4be70ceb3ed97
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-06-30 14:17:53 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,16 +1,3 @@
-function find_lcd(denom1, denom2){
-    let lcd=1;
-    for(let itr=Math.min(denom1, denom2); itr<=denom1*denom2; itr++){
-        if( Math.floor(itr%denom1)===0&&Math.floor(itr%denom2)===0){
-            lcd=itr;
-            
-            break;
-        }
-    }
-    
-    return lcd;
-}
-
 function find_small(num1, num2){
     let lcd=1;
     for(let itr=Math.min(num1, num2); itr>=1; itr--){
@@ -23,16 +10,10 @@ function find_small(num1, num2){
     return lcd;
 }
 
-function get_matrix_value(denom, lcd){
-    return (lcd/denom)
-    
-}
-
 function solution(numer1, denom1, numer2, denom2) {
   const lcd=denom1*denom2;
-  let answer=[numer1*(lcd/denom1)+numer2*(lcd/denom2), lcd];
+  let answer=[denom1*numer2+denom2*numer1, lcd];
     
-    console.log(answer)
   const answer_lcd=find_small(answer[0], lcd)
   return [answer[0]/answer_lcd, lcd/answer_lcd]
 }
