1da2446897a9167029b4e6c0b307bb57809e8576
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-06-30 14:24:40 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,19 +1,13 @@
-function find_small(num1, num2){
-    let lcd=1;
-    for(let itr=Math.min(num1, num2); itr>=1; itr--){
-        if( Math.floor(num1%itr)===0&&Math.floor(num2%itr)===0){
-            lcd=itr;
-            break;
-        }
-    }
-    
-    return lcd;
+// 24,16 = 16,8 = 8,0
+function find_gcd(num1, num2){
+    // 0이 될때까지 나눈다.
+    return num2===0? num1: find_gcd(num2, num1%num2)
 }
 
 function solution(numer1, denom1, numer2, denom2) {
-  const lcd=denom1*denom2;
-  let answer=[denom1*numer2+denom2*numer1, lcd];
-    
-  const answer_lcd=find_small(answer[0], lcd)
-  return [answer[0]/answer_lcd, lcd/answer_lcd]
+  const denom=denom1*denom2;
+    const number=denom1*numer2+denom2*numer1;
+
+  const gcd=find_gcd(number, denom)
+  return [number/gcd, denom/gcd]
 }
