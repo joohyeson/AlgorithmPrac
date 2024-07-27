1b0ba9c12994377b1315965ad42bef048d4df3cb
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-07-27 22:52:52 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 62.72 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,55 +1,24 @@
-function getAnswer(arr, an){
-    const wh=arr.length/2
-    
-    // 네 구역으로 분리
-    const area=[[[0, wh], [0, wh]], [[wh, 2*wh], [0,wh]], [[0,wh], [wh, 2*wh]],
-                [[wh, 2*wh], [wh, 2*wh]]];
-    
-    area.forEach(([[xStart, xEnd], [yStart, yEnd]])=>{
-        let isSame=true;
-        let prev=-1
-         for(let i=xStart; i<xEnd; i++){
-            for(let j=yStart; j<yEnd; j++){
-                if(prev!==-1){
-                    if(prev!==arr[i][j]){
-                        isSame=false;
-                        break;
-                    }
-                    
-                }
-                prev=arr[i][j]
-                
-            }    
-        }
-        
-        if(isSame){
-            an[prev]+=1
-        }else{
-            getAnswer(arr.slice(xStart, xEnd).map((a)=>a.slice(yStart, yEnd)), an)
-        }
-    })
-    
-    return an;
-}
 
-
-function solution(arr) {
-    let prev=-1
-    let isSame=true;
-    arr.forEach((a)=>a.forEach((b)=> {
-        if(prev!==-1&&prev!==b){
-            isSame=false;
-            
-        }
-        prev=b
-    }))
-    
-    if(isSame){
-        return prev===1? [0,1]: [1,0]
-    }
-    
+function solution(arr) {    
     // 4^n씩 묶는다.
     // 각 범위에 들어가는 숫자들이 모두 같다면 +1, 아니라면 더 작은 범위 탐색
     // 가장 큰 범위부터 안으로 들어가면서 실행
-    return getAnswer(arr, [0,0]);
+   const getAnswer=(row, col, an)=>{
+       if(an<2) return arr[row][col]===0? [1,0]:[0,1]
+       
+        const wh=an/2;
+        const area=[[0,0], [0,1], [1,0],[1,1]];
+        let cnt=[0,0]
+        
+        area.forEach(([x, y])=>{
+            const [cx,cy]=getAnswer(row+x*wh, col+y*wh, wh)
+            cnt[0]+=cx;
+            cnt[1]+=cy;
+        })
+       
+       if(cnt[0]===0) return [0,1];
+       if(cnt[1]===0) return [1,0];
+       return cnt;
+    }
+    return getAnswer(0,0, arr.length);
 }
