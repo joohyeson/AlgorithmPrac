bb07744539dfe10bdc7826e0f39e987a88ebd246
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-07-27 14:01:25 +0900

    [level 0] Title: 가까운 수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,9 +1,3 @@
 function solution(array, n) {
-let obj={}
-   array.map((num, i)=>Object.assign(obj, {[num]: Math.abs(num-n)}) )
-    const arr=Object.values(obj).sort((a,b)=>a-b)
-    const ans=Object.keys(obj)[Object.values(obj).indexOf(arr[0])]
-
-    return  Number(ans)
-    
+return array.sort((a,b)=>Math.abs(a-n) - Math.abs(b-n)|| (a-b))[0]    
 }
