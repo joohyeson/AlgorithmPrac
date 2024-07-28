10ff68b70c86edd8dcc34108b77469c12508f910
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-07-28 11:54:55 +0900

    [level 1] Title: 직사각형 별찍기, Time: 69.98 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    
+    for(let i=0; i<b; i++){
+        let ans=""
+        for(let j=0; j<a; j++){
+            ans+="*"
+        }
+        
+        console.log(ans)
+    }
+});
