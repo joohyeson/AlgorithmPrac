1d99982deb689d485b86aed3c1d37fcd61a427e5
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-07-28 20:56:46 +0900

    [level 0] Title: 평행, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.js
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -4,14 +4,13 @@ function solution(dots) {
     const d3=[[0,3],[1,2]];
     const maps=[d1,d2, d3]
     let ans=0;
-    maps.map((map)=>{
-        const [[x1,x2],[y1,y2]]=map
-        console.log(((dots[x2][1]-dots[x1][1])/(dots[x2][0]-dots[x1][0])));
-        console.log(((dots[y2][1]-dots[y1][1])/(dots[y2][0]-dots[y1][0])))
-        if(((dots[x2][1]-dots[x1][1])/(dots[x2][0]-dots[x1][0]))===((dots[y2][1]-dots[y1][1])/(dots[y2][0]-dots[y1][0]))){
-        ans++;
-    }
     
-    } )
-    return ans?1:0;
+    for(let i=0; i<maps.length; i++){
+        const [[x1,x2],[y1,y2]]=maps[i]
+        if(((dots[x2][1]-dots[x1][1])/(dots[x2][0]-dots[x1][0]))===((dots[y2][1]-dots[y1][1])/(dots[y2][0]-dots[y1][0]))){
+        return 1;
+    }
+    }
+   
+    return 0;
 }
