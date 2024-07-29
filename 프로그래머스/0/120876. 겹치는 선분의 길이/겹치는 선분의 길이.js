71bd44c7f191599ce40a6539743bf9dfa3a8d97a
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-07-29 18:01:07 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,14 +1,11 @@
 function solution(lines) {
-    let obj={}
-    for(let i=0; i<=200; i++){
-        Object.assign(obj,{[i]:0});
-    }
+   let line = new Array(200).fill(0);
     
-    lines.map((line)=>{
-        for(let i=line[0]; i<line[1]; i++){
-            obj[i+100]++;
+    lines.map((l)=>{
+        for(let i=l[0]; i<l[1]; i++){
+            line[i+100]++;
         }
     })
     
-    return Object.values(obj).filter((o)=> o>=2).length;
+    return line.filter((o)=> o>=2).length;
 }
