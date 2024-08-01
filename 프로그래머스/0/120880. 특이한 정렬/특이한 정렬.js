ed5182484e17d273aef18f8475f14a622c18b091
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-08-01 19:05:11 +0900

    [level 0] Title: 특이한 정렬, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,5 +1,5 @@
 function solution(numlist, n) {
     
-    return numlist.sort((a,b)=> a-b).sort((a, b)=> Math.abs(a-n)>Math.abs(b-n)?1: -1)
+    return numlist.sort((a, b)=> Math.abs(a-n)-Math.abs(b-n)||b-a)
     
 }
