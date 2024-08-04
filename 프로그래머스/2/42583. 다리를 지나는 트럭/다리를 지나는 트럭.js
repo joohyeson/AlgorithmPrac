65ff20d6ec150ec43cad1a197c227f922b124651
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-08-05 01:00:50 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 106.76 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,31 @@
+function solution(bridge_length, weight, truck_weights) {
+    const arr=truck_weights.map((weight)=> [weight, bridge_length])
+    let onb=[]
+    var answer = 0;
+    let nowWeight=0;
+    
+    while(arr.length>0){
+        answer++;
+        onb.forEach((t)=> t[1]--)
+        onb=onb.filter((t)=> t[1]!==0);
+        
+        const target=arr.shift();
+        
+        if((onb.reduce((acc, cur)=> acc+cur[0],0)+target[0])>weight||onb.length>=bridge_length){
+            arr.unshift(target)
+        }else{
+            onb.push(target);
+        }
+        
+            //console.log(arr, onb, answer)
+    }
+    
+    console.log(onb, answer)
+
+    
+    let max=0;
+    for(let i of onb){
+        max=Math.max(max, i[1])
+    }
+    return answer+max;
+}
