c31f8fa4f4fa25d515345f5b3cd236f1895c65bd
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-08-10 15:36:08 +0900

    [level 2] Title: 소수 찾기, Time: 160.83 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,44 @@
+function solution(numbers) {
+    let arr=[...numbers];
+    let answers=[];
+    let visited=Array(arr.length).fill(0)
+    const test=(str)=>{
+        if(str!==''){
+            answers.push(str)
+        }
+        if(str.length===arr.length){
+            return 
+        }
+        
+        for(let i=0; i<arr.length; i++){
+            if(visited[i]===0){
+                visited[i]=1;
+                test(str+arr[i]);
+                visited[i]=0;
+            }
+        }
+    }
+    test('')
+    
+    const a=[...new Set(answers.map((s)=> Number(s)))]
+    
+    // 소수찾기
+    let count=0;
+    
+    a.forEach((num)=>{
+        if(num===0||num===1){
+            count++;
+        }
+        for(let i=2; i<num; i++){
+            if(num%i===0){
+                count++;
+                break;
+            }
+        }
+    })
+    
+    console.log(a, count)
+    
+   
+    return  a.length-count;
+}
