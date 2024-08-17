7b3b64f3c2e4687370cecd7be298226ce4d55603
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-08-17 14:58:27 +0900

    [level 2] Title: 모음 사전, Time: 5.28 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,13 +1,11 @@
 function solution(word) {
-    // length별로 저장
-    let arr=[['A', 'E', 'I', 'O', 'U'],[],[],[],[]]
     let a=['A', 'E', 'I', 'O', 'U']
+    let arr=[[...a]]
+
     
     
     for(let length=1; length<=4; length++){
-        arr[length]=arr[length-1].flatMap((s)=> a.flatMap((r)=> {
-            return s+r
-        })) 
+        arr.push(arr[length-1].flatMap((s)=> a.flatMap((r)=> s+r)))
     }
 
     return [...new Set(arr.join().split(","))].sort((a, b)=> a>b? 1:-1).indexOf(word)+1;
