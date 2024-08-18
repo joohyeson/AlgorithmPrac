85357260237004ee7895f8f11c4af8651ebf6a67
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-08-18 16:48:29 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,9 @@
+function solution(n)
+{
+    var answer = 0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    //console.log('Hello Javascript')
+
+    return n.toString().split("").reduce((acc, cur)=> acc+Number(cur),0);
+}
