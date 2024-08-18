ce87b4a0242a09e6900c483d44c8ac1318dc8963
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-08-18 17:17:13 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,4 +1,4 @@
 function solution(phone_number) {
     var answer = '';
-    return ("*").repeat(phone_number.length-4)+phone_number.slice(phone_number.length-4);
+    return ("*").repeat(phone_number.length-4)+phone_number.slice(-4);
 }
