ef2586496b35c822a7d7d7426de64a3a6dbd888a
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-08-18 18:10:16 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,4 +1,4 @@
 function solution(n) {
     var answer = 0;
-    return +parseInt(n.toString(3).split("").reverse().join(""),3).toString();
+    return parseInt([...n.toString(3)].reverse().join(""),3);
 }
