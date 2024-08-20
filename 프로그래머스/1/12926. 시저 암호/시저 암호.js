6258949884d8140b5eaf4b2f3a4ae648860f8465
Juhye Son <68503845+joohyeson@users.noreply.github.com>
2024-08-20 22:55:31 +0900

    [level 1] Title: 시저 암호, Time: 2.37 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,29 +1,11 @@
 function solution(s, n) {
-    
+    const upper='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const lower='abcdefghijklmnopqrstuvwxyz'
     var answer = s.split(" ").map((s)=> s.split("").map((s)=>{
-        const cur=s.charCodeAt()
-        if(cur>="a".charCodeAt()){
-             let ans=String.fromCharCode(cur+n).charCodeAt()
-            let last="z".charCodeAt()
-            
-            //console.log(cur, n, ans, last)
-            if(ans>last){
-                return String.fromCharCode(ans-26)
-            }else{
-                return String.fromCharCode(ans)
-            }
-
-        }else{
-            let ans=String.fromCharCode(cur+n).charCodeAt()
-            let last="Z".charCodeAt()
-            
-           // console.log(cur, s, ans, last)
-            if(ans>last){
-                return String.fromCharCode(ans-26)
-            }else{
-                return String.fromCharCode(ans)
-            }
-        }
+        const arr=upper.includes(s)? upper: lower;
+        const idx=(arr.indexOf(s)+n)%arr.length;
+        
+       return arr[idx]
     }).join("")).join(" ");
     return answer;
 }
